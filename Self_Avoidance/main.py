7e4b9864b0fb57a00341ca5593e3a3d9cb17ff51
ldt20 <Ldt20020517@bjfu.edu.cn>
2023-12-19 17:24:57 +0800

    23.12.19版本，是无人机自动避障的主文件，现在可以正常训练，但是所有的无人机全部撞毁，需要查找原因

--- Self_Avoidance/main.py
+++ Self_Avoidance/main.py
@@ -15,13 +15,18 @@ import random
 
 
 if __name__ == '__main__':
+    # pth文件保存的位置
+    pth_load = {'actor': r'D:\PythonProject\Drone_self_avoidance\Self_Avoidance\actor.pth',
+                'critic': r'D:\PythonProject\Drone_self_avoidance\Self_Avoidance\critic.pth',
+                'target_actor': r'D:\PythonProject\Drone_self_avoidance\Self_Avoidance\target_actor.pth',
+                'target_critic': r'D:\PythonProject\Drone_self_avoidance\Self_Avoidance\target_critic.pth'}
     # 策略网络学习率
     actor_lr = 5e-4
     # 价值网络学习率
     critic_lr = 5e-3
     # 迭代次数
-    num_episodes = 1000
-    # 隐藏节点
+    num_episodes = 10000
+    # 隐藏节点，先暂定64，后续可以看看效果
     hidden_dim = 64
     # 折扣因子
     gamma = 0.98
@@ -30,45 +35,38 @@ if __name__ == '__main__':
     # 经验回放池大小
     buffer_size = 10000
     # 经验回放池最小经验数目
-    minimal_size = 1000
+    minimal_size = 100
     # 每一批次选取的经验数量
     batch_size = 64
     # 高斯噪声标准差
     sigma = 0.01
-    # 三维环境下动作
-    state_dim = 3
+    # 三维环境下动作，加上一堆状态的感知，目前是124+16=140个
+    state_dim = 140
     # 暂定直接控制智能体的位移，所以是三维的
     action_dim = 3
+    # 每一次迭代中，无人机的数量
+    num_uavs = 30
+    # 无人机可控风速
+    v0 = 40
     # 设备
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
-    """生成一下障碍物:球体(半球)，立方体"""
-    obstacle = {"sphere": ([[20, 30, 0], 16], [[70, 25, 60], 18]),
-                "cube": ([[40, 40, 0], [50, 50, 100]],
-                         [[5, 80, 0], [95, 95, 50]],
-                         [[85, 5, 0], [95, 70, 40]])}
-    # 智能体的初始化位置
-    agent_state = np.array([10.0, 10.0, 15.0])
     # 智能体的半径（先暂时定义为球体）
     agent_r = 1
-    # 目标区域(对角线)
-    target_area = np.array([[92, 92, 92], [98, 98, 98]])
     # 动作区域
-    action_area = np.array([[0, 0, 0], [100, 100, 100]])
+    action_area = np.array([[0, 0, 0], [100, 100, 25]])
     # 动作最大值
-    action_bound = 0.05
+    action_bound = 0.5
 
     # 实例化交互环境
-    env = environment.Environment(obstacle, agent_state, agent_r, target_area, action_area)
-    random.seed(0)
-    torch.manual_seed(0)
+    env = environment.Environment(agent_r, action_area, num_uavs, v0)
     # 实例化经验回放池
     replay_buffer = tools.ReplayBuffer(buffer_size)
 
     # 实例化DDPG对象，其实动作为非离散，所以为False
     agent = DDPG(state_dim, action_dim, state_dim + action_dim, hidden_dim, False,
                  action_bound, sigma, actor_lr, critic_lr, tau, gamma, device)
-    return_list = tools.train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size)
+    return_list = tools.train_off_policy_agent(env, agent, num_episodes, replay_buffer, minimal_size, batch_size, pth_load)
 
     # 绘图
     episodes_list = list(range(len(return_list)))
