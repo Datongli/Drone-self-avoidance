b851f6bb044b10330c1458b92716a7a51e24e3f1
ldt20 <Ldt20020517@bjfu.edu.cn>
2024-01-09 16:33:55 +0800

    24.1.9，在输入和每一层全连接之间增加了bn层的DDPG网络

--- Self_Avoidance/DDPG.py
+++ Self_Avoidance/DDPG.py
@@ -6,13 +6,12 @@ from torch import nn
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 import torch.nn.init as init
+import torch.nn.utils.weight_norm as weight_norm
+# import torch.nn.utils.parametrizations.weight_norm as weight_norm
 import tools
 
 
-class LayerFC_actor(torch.nn.Module):
-    """
-    用于actor的网络
-    """
+class LayerFC(torch.nn.Module):
     def __init__(self, num_in, num_out, hidden_dim, activation=torch.tanh, out_fn=lambda x: x):
         """
         初始化这个两层神经网络
@@ -24,77 +23,22 @@ class LayerFC_actor(torch.nn.Module):
         例如，如果你创建了一个 LayerFC 类的实例时提供了一个不同的输出函数，比如 out_fn=lambda x: torch.sigmoid(x)，
         那么在前向传播过程中就会应用该函数来处理神经网络的输出。这使得 LayerFC 类更加灵活，能够适应不同的需求。
         """
-        super(LayerFC_actor, self).__init__()
-        self.layer1 = torch.nn.Sequential(
-            torch.nn.Linear(num_in, 32),
-            # torch.nn.BatchNorm1d(32),
-            torch.nn.PReLU()
-        )
-        self.layer2 = torch.nn.Sequential(
-            torch.nn.Linear(32, 64),
-            # torch.nn.BatchNorm1d(64),
-            torch.nn.PReLU()
-        )
-        self.layer3 = torch.nn.Sequential(
-            torch.nn.Linear(64, 128),
-            # torch.nn.BatchNorm1d(128),
-            torch.nn.PReLU()
-        )
-        self.layer4 = torch.nn.Sequential(
-            torch.nn.Linear(128, hidden_dim),
-            # torch.nn.BatchNorm1d(hidden_dim),
-            torch.nn.PReLU()
-        )
-        self.fc5 = torch.nn.Linear(hidden_dim, num_out)
+        super(LayerFC, self).__init__()
+        self.bn0 = nn.BatchNorm1d(num_in)
+        self.fc1 = weight_norm(torch.nn.Linear(num_in, 128))
+        self.bn1 = nn.BatchNorm1d(128)
+        self.fc2 = weight_norm(torch.nn.Linear(128, 256))
+        self.bn2 = nn.BatchNorm1d(256)
+        self.fc3 = weight_norm(torch.nn.Linear(256, 128))
+        self.bn3 = nn.BatchNorm1d(128)
+        self.fc4 = weight_norm(torch.nn.Linear(128, hidden_dim))
+        self.bn4 = nn.BatchNorm1d(hidden_dim)
+        self.fc5 = weight_norm(torch.nn.Linear(hidden_dim, num_out))
         self.activation = activation
+        self.prelu = nn.PReLU()
         self.out_fn = out_fn
-        # self.scale_factor = nn.Parameter(torch.tensor(20.0), requires_grad=True)
-        self.init_weights()
-
-    def init_weights(self):
-        # 使用 Xavier/Glorot 初始化
-        for layer in [self.layer1[0], self.layer2[0], self.layer3[0], self.layer4[0], self.fc5]:
-            if isinstance(layer, nn.Linear):
-                init.xavier_uniform_(layer.weight)
-                init.zeros_(layer.bias)
-
-    def forward(self, x):
-        # print("x:{}".format(x))
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        print("$" * 100)
-        print("PRelu(self.fc4):{}".format(x))
-        print("self.fc5:{}".format(self.fc5(x)))
-        x = self.out_fn(self.fc5(x))
-        print("输出x为：{}".format(x))
-        return x
-
-
-class LayerFC_critic(torch.nn.Module):
-    """
-    用于critic的网络
-    """
-    def __init__(self, num_in, num_out, hidden_dim, activation=torch.tanh, out_fn=lambda x: x):
-        """
-        初始化这个两层神经网络
-        :param num_in: 输入的节点数量
-        :param num_out: 输出的节点数量
-        :param hidden_dim: 隐藏层节点数量
-        :param activation: 激活函数
-        :param out_fn: 输出的函数。这里使用了一个匿名函数 (lambda 函数)，该函数接受一个参数 x，并直接返回 x。
-        例如，如果你创建了一个 LayerFC 类的实例时提供了一个不同的输出函数，比如 out_fn=lambda x: torch.sigmoid(x)，
-        那么在前向传播过程中就会应用该函数来处理神经网络的输出。这使得 LayerFC 类更加灵活，能够适应不同的需求。
-        """
-        super(LayerFC_critic, self).__init__()
-        self.fc1 = torch.nn.Linear(num_in, 32)
-        self.fc2 = torch.nn.Linear(32, 64)
-        self.fc3 = torch.nn.Linear(64, 128)
-        self.fc4 = torch.nn.Linear(128, hidden_dim)
-        self.fc5 = torch.nn.Linear(hidden_dim, num_out)
-        self.activation = activation
-        self.out_fn = out_fn
+        self.train = True
+        self.scale_factor = nn.Parameter(torch.tensor([random.random() for _ in range(3)]), requires_grad=True)
         self.init_weights()
 
     def init_weights(self):
@@ -105,11 +49,32 @@ class LayerFC_critic(torch.nn.Module):
                 init.zeros_(layer.bias)
 
     def forward(self, x):
-        x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.activation(self.fc3(x))
-        x = self.activation(self.fc4(x))
+        if self.train:
+            x = self.bn0(x)
+        x = self.fc1(x)
+        if self.train:
+            x = self.bn1(x)
+        x = self.prelu(x)
+        x = self.fc2(x)
+        if self.train:
+            x = self.bn2(x)
+        x = self.prelu(x)
+        x = self.fc3(x)
+        if self.train:
+            x = self.bn3(x)
+        x = self.prelu(x)
+        x = self.fc4(x)
+        if self.train:
+            x = self.bn4(x)
+        x = self.prelu(x)
+        # print("$" * 100)
+        # print("PRelu(self.fc4):{}".format(x))
+        # print("self.fc5:{}".format(self.fc5(x)))
+        # 乘以一个自适应的放缩
         x = self.out_fn(self.fc5(x))
+        # x = self.out_fn(self.fc5(x) * self.scale_factor.unsqueeze(0))
+        # print("自适应放缩为：{}".format(self.scale_factor))
+        # print("输出x为：{}".format(x))
         return x
 
 
@@ -118,7 +83,8 @@ class DDPG:
     DDPG算法
     """
     def __init__(self, num_in_actor, num_out_actor, num_in_critic, hidden_dim, discrete,
-                 action_bound, sigma, actor_lr, critic_lr, tau, gamma, max_eps_episode, min_eps, device):
+                 action_bound, sigma, actor_lr, critic_lr, tau, gamma, max_eps_episode, min_eps,
+                 regularization_strength, device):
         """
         初始化DDPG算法
         :param num_in_actor:策略网络输入节点（状态维度）
@@ -134,16 +100,15 @@ class DDPG:
         :param gamma:折扣因子
         :param max_eps_episode:最大贪心次数
         :param min_eps:最小贪心概率
+        :param regularization_strength:正则化强度
+        :param batch_size:批量数量
         :param device:设备
         """
         out_fn = (lambda x: x) if discrete else (lambda x: torch.tanh(x) * action_bound)
-        # self.actor = LayerFC(num_in_actor, num_out_actor, hidden_dim, activation=F.relu, out_fn=out_fn).to(device)
-        # self.target_actor = LayerFC(num_in_actor, num_out_actor, hidden_dim, activation=F.relu, out_fn=out_fn).to(device)
-        self.actor = LayerFC_actor(num_in_actor, num_out_actor, hidden_dim, activation=torch.tanh, out_fn=out_fn).to(device)
-        self.target_actor = LayerFC_actor(num_in_actor, num_out_actor, hidden_dim, activation=torch.tanh, out_fn=out_fn).to(device)
-        # 这里的输出开始是有问题的，输出了3个结点
-        self.critic = LayerFC_critic(num_in_critic, 1, hidden_dim, activation=nn.PReLU()).to(device)
-        self.target_critic = LayerFC_critic(num_in_critic, 1, hidden_dim, activation=nn.PReLU()).to(device)
+        self.actor = LayerFC(num_in_actor, num_out_actor, hidden_dim, activation=torch.tanh, out_fn=out_fn).to(device)
+        self.target_actor = LayerFC(num_in_actor, num_out_actor, hidden_dim, activation=torch.tanh, out_fn=out_fn).to(device)
+        self.critic = LayerFC(num_in_critic, 1, hidden_dim, activation=nn.PReLU()).to(device)
+        self.target_critic = LayerFC(num_in_critic, 1, hidden_dim, activation=nn.PReLU()).to(device)
         # 初始化目标价值网络并设置和价值网络相同的参数
         self.target_critic.load_state_dict(self.critic.state_dict())
         # 初始化目标策略网络并设置和策略相同的参数
@@ -158,6 +123,7 @@ class DDPG:
         self.action_dim = num_out_actor
         self.max_eps_episode = max_eps_episode
         self.min_eps = min_eps
+        self.regularization_strength = regularization_strength
         self.device = device
         self.step = 0
         self.train = True
@@ -176,13 +142,16 @@ class DDPG:
         :param state:状态值
         :return: 动作值
         """
-        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        # 如果是网络的训练模式
+        if self.actor.train:
+            state = torch.tensor(state, dtype=torch.float).to(self.device)
+        # 如果是网络的应用模式
+        else:
+            state = torch.tensor(np.array([state]), dtype=torch.float).to(self.device)
         action = self.actor(state).detach().cpu().numpy()
         # 给动作添加噪声，增加搜索
         # 正态分布产生随机数并乘以标准差
         action = action + self.sigma * np.random.randn(self.action_dim)
-        # print("=" * 100)
-        # print(action)
         return np.array(action)
 
     def take_action(self, state):
@@ -239,21 +208,15 @@ class DDPG:
         """
         # 从过渡态字典中得到相应的值
         states = torch.tensor(transition_dict['states'], dtype=torch.float).to(self.device)
-        actions = torch.tensor(transition_dict['actions'], dtype=torch.float).to(self.device)
+        actions = torch.tensor(np.array(transition_dict['actions']), dtype=torch.float).to(self.device)
         rewards = torch.tensor(transition_dict['rewards'], dtype=torch.float).view(-1, 1).to(self.device)
         next_states = torch.tensor(transition_dict['next_states'], dtype=torch.float).to(self.device)
         dones = torch.tensor(transition_dict['dones'], dtype=torch.float).view(-1, 1).to(self.device)
-
-        # 见笔记上的注释
+        # 将网络设置为训练模式
         next_q_values = self.target_critic(torch.cat([next_states, self.target_actor(next_states)], dim=1))
         q_targets = rewards + self.gamma * next_q_values * (1 - dones)
-        # print("rewards:{}".format(rewards[:4].view(1, -1)))
-        # print("q_targets:{}".format(q_targets[:4].view(1, -1)))
-        # print("next_q_values:{}".format(next_q_values[:4].view(1, -1)))
         # 计算并更新价值网络的损失
         critic_loss = torch.mean(F.mse_loss(self.critic(torch.cat([states, actions], dim=1)), q_targets))
-        # print("critic_value:{}".format(self.critic(torch.cat([states, actions], dim=1))[:4].view(1, -1)))
-        # print("critic_loss:{}".format(critic_loss))
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         # 梯度裁剪，可以在这里添加
@@ -264,6 +227,9 @@ class DDPG:
         # 见笔记上的注释
         # 计算并更新策略网络的损失
         actor_loss = - torch.mean(self.critic(torch.cat([states, self.actor(states)], dim=1)))  # 策略网络为了使得Q值最大化
+        # print("原始actor_loss:{}".format(actor_loss))
+        actor_loss += self.regularization_strength * sum(p.pow(2).sum() for p in self.actor.parameters())
+        # print("增加了正则化之后的actor_loss:{}".format(actor_loss))
         self.actor_optimizer.zero_grad()
         actor_loss.backward()
         # 梯度裁剪，可以在这里添加
