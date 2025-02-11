e420175e5a634614eb57049b35855a75d1c438ed
Datongli <ldt20020517@163.com>
2025-02-11 14:53:12 +0800

    删除了numpy需要对应的版本信息

--- self_avoidance_gym/environment-gym/setup.py
+++ self_avoidance_gym/environment-gym/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="uav_avoid_gym",
     version="0.0.1",
-    install_requires=["gymnasium==0.29.1", "numpy==1.24.1", "matplotlib==3.8.2"],
+    install_requires=["gymnasium==0.29.1", "matplotlib==3.8.2"],
 )
